43f08dfac83c12458e918e7f73aa2d941f779311
janikibichi <janikibichi@gmail.com>
2018-05-25 18:09:06 +0300

    Return future from for-comprehension

--- src/main/scala/com/github/janikibichi/learnakka/futures/ForComprehensionsForFutures.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/ForComprehensionsForFutures.scala
@@ -1,5 +1,18 @@
 package com.github.janikibichi.learnakka.futures
 
+import scala.concurrent.duration._
+import scala.concurrent.{Await,Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object ForComprehensionsForFutures extends App {
+  val futureA = Future(20+30)
+  val futureB = Future(30+30)
+  
+  val finalFuture: Future[Int] = for{
+    a <- futureA
+    b <- futureB
+  } yield a+b
+
+  println("Future result is " +Await.result(finalFuture, 1 seconds))
 
 }
