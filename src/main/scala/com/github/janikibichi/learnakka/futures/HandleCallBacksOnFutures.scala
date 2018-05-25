d9f5a699a9ec2594453bc27298f269f0b00ce3c9
janikibichi <janikibichi@gmail.com>
2018-05-25 18:22:31 +0300

    wait for callback

--- src/main/scala/com/github/janikibichi/learnakka/futures/HandleCallBacksOnFutures.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/HandleCallBacksOnFutures.scala
@@ -1,5 +1,22 @@
 package com.github.janikibichi.learnakka.futures
 
-object HandleCallBacksOnFutures extends App{
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
+
+object HandleCallBacksOnFutures extends App{
+  val future = Future(1+2).mapTo[Int]
+
+  //Callback
+  future onComplete {
+    case Success(result) =>
+      println(s"result id $result")
+
+    case Failure(fail) =>
+      fail.printStackTrace()
+  }
+
+  //Print
+  println("This is executed before callback")
 }
