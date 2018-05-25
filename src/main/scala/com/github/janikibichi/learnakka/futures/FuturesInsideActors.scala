4ff7a36a62a1ad31eb5c8c89a2e5148b60d5b30d
janikibichi <janikibichi@gmail.com>
2018-05-25 17:56:02 +0300

    updated stream

--- src/main/scala/com/github/janikibichi/learnakka/futures/FuturesInsideActors.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/FuturesInsideActors.scala
@@ -1,5 +1,27 @@
 package com.github.janikibichi.learnakka.futures
 
-object FuturesInsideActors extends App {
+import akka.actor.{Props, ActorSystem,Actor}
+import scala.concurrent.{Await,Future}
+import scala.concurrent.duration._
 
+object FuturesInsideActors extends App {
+  val actorSystem = ActorSystem("FutureInside")
+
+  //Create and define the Actor in ActorSystem
+  val fActor = actorSystem.actorOf(Props[FutureInsideActor])
+
+  //Send Message to Actor
+  fActor ! (10,20)
+}
+
+//Create and Actor that uses a future inside
+class FutureInsideActor extends Actor{
+  import context.dispatcher
+
+  def receive ={
+    case(a: Int, b: Int) =>
+      val f = Future(a+b)
+      (Await.result(f,10 seconds))
+      println(s"Value generated $f")
+  }
 }
