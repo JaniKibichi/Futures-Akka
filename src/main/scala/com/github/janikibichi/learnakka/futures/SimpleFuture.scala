6b98d031c82fd3514d33ab42dbe2d5fb7f373e86
janikibichi <janikibichi@gmail.com>
2018-05-25 17:14:13 +0300

    update readme

--- src/main/scala/com/github/janikibichi/learnakka/futures/SimpleFuture.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/SimpleFuture.scala
@@ -1,5 +1,13 @@
 package com.github.janikibichi.learnakka.futures
 
+import scala.concurrent.duration._
+import scala.concurrent.{Await,Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object SimpleFuture extends App {
-  
+  val future = Future(1+2).mapTo[Int]
+
+  val sum = Await.result(future, 10 seconds)
+
+  println(s"Future Result $sum")
 }
