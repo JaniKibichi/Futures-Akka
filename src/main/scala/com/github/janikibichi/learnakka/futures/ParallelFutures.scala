429465fb4810e1b4ef2d4a2ff7a477252fb6db9a
janikibichi <janikibichi@gmail.com>
2018-05-25 18:51:52 +0300

    added seq vs. parallel

--- src/main/scala/com/github/janikibichi/learnakka/futures/ParallelFutures.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/ParallelFutures.scala
@@ -1,5 +1,46 @@
 package com.github.janikibichi.learnakka.futures
 
-object ParallelFutures extends App{
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await,Future}
 
+object ParallelFutures extends App{
+  import Fib._
+
+  //Sequential
+  val t1 = System.currentTimeMillis()
+  val sum = fib(100) + fib(100) + fib(100)
+
+  println(s"sum is $sum time taken in sequential computation ${System.currentTimeMillis()-t1/1000.0}")
+
+  //Parallel
+  val t2 = System.currentTimeMillis()
+  val future1 = Future(fib(100))
+  val future2 = Future(fib(100))
+  val future3 = Future(fib(100))
+
+  val future = for{
+    x <- future1
+    y <- future2
+    z <- future3
+  }yield (x+y+z)
+
+  future.onSuccess{
+    case sum =>
+      val endTime =((System.currentTimeMillis()-t2)/1000.0)
+      println(s"sum is $sum time taken in parallel computation $endTime seconds")
+  }
+  Thread.sleep(5000)
+}
+
+//An object to create the Fibonacci number
+object Fib{
+  def fib(n: Int): Int ={
+
+    def fib_tail(n: Int, a: Int, b: Int): Int = n match {
+      case 0 => a
+      case _ => fib_tail(n-1,b,a+b)
+    }
+
+    fib_tail(n,0,1)
+  }
 }
