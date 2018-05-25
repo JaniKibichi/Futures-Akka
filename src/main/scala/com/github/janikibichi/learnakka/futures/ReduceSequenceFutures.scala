a625cd061215a68452ee15f19596b2ab118c956b
janikibichi <janikibichi@gmail.com>
2018-05-25 19:10:31 +0300

    updated the readme

--- src/main/scala/com/github/janikibichi/learnakka/futures/ReduceSequenceFutures.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/ReduceSequenceFutures.scala
@@ -1,5 +1,15 @@
 package com.github.janikibichi.learnakka.futures
 
-object ReduceSequenceFutures extends App {
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.{Await,Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
+object ReduceSequenceFutures extends App {
+  val timeout = Timeout(10 seconds)
+  val listOfFutures = (1 to 10).map(Future(_))
+
+  val finalFuture = Future.reduce(listOfFutures)(_+_)
+
+  println(s"sum of numbers from 1 to 10 is ${Await.result(finalFuture, 10 seconds)}")
 }
