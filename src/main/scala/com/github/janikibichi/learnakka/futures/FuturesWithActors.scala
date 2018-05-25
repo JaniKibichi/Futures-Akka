1d24a79e44e1cfb3fcc7704d370a2f48d9e3eb69
janikibichi <janikibichi@gmail.com>
2018-05-25 17:36:33 +0300

    add future actor

--- src/main/scala/com/github/janikibichi/learnakka/futures/FuturesWithActors.scala
+++ src/main/scala/com/github/janikibichi/learnakka/futures/FuturesWithActors.scala
@@ -1,5 +1,30 @@
 package com.github.janikibichi.learnakka.futures
 
-object FuturesWithActors extends App {
+import akka.actor.{ActorSystem,Actor,Props}
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
+object FuturesWithActors extends App {
+  implicit val timeout = Timeout(10 seconds)
+
+  val actorSystem = ActorSystem("FutureActors")
+
+  //Create and Define the Actor in the ActorSystem
+  val computationActor = actorSystem.actorOf(Props[ComputationActor])
+
+  //Ask the Computation Actor to deliver calculation result
+  val future = (computationActor ? (2,3)).mapTo[Int]
+  val sum = Await.result(future, 10 seconds)
+
+  //Print the result
+  println(s"Future Result $sum")
+}
+
+//Create an Actor that calculates the sum of two integers and delivers it back to sender
+class ComputationActor extends Actor{
+  def receive = {
+    case (a: Int, b: Int) => sender ! (a+b)
+  }
 }
